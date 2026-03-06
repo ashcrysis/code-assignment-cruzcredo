12929515941c5f40ede1c71848c80b79174237e7
Asher Martins <109311931+ashcrysis@users.noreply.github.com>
2026-03-06 09:28:36 -0300

    feat: main menu graphics

---
+++ Assets/Scripts/UI/UIIdleRotate.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class UIIdleRotate : MonoBehaviour
+{
+    public float angle = 5f;  
+    public float speed = 2f;  
+
+    private Quaternion startRotation;
+
+    void Start()
+    {
+        startRotation = transform.rotation;
+    }
+
+    void Update()
+    {
+        float z = Mathf.Sin(Time.time * speed) * angle;
+        transform.rotation = startRotation * Quaternion.Euler(0f, 0f, z);
+    }
+}
